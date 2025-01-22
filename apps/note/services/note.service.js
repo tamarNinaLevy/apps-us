658e4356a05bdee484e020716a142730ccd39d0d
Nadav Botzer <botzer14@gmail.com>
2025-01-22 13:02:41 +0200

    Add note service

--- apps/note/services/note.service.js
+++ apps/note/services/note.service.js
@@ -1,1 +1,75 @@
 // note service
+import { storageService } from 'services/async-storage.service.js'
+import { loadFromStorage, saveToStorage } from 'services/util.service.js'
+
+const NOTE_KEY = 'noteDB'
+_createNotes()
+
+function query(filterBy = {}) {
+    return storageService.query(NOTE_KEY)
+}
+
+function get(noteId) {
+    return storageService.get(NOTE_KEY, noteId)
+}
+
+function remove(noteId) {
+    return storageService.remove(NOTE_KEY, noteId)
+}
+
+function save(note) {
+    if (note.id) {
+        return storageService.put(NOTE_KEY, note)
+    } else {
+        return storageService.post(NOTE_KEY, note)
+    }
+}
+
+function _createNotes() {
+    console.log('creating notes')
+    let notes = loadFromStorage(NOTE_KEY)
+
+    if (!notes || !notes.length) {
+        const notes = [
+            {
+                id: 'n101',
+                createdAt: 1112222,
+                type: 'NoteTxt',
+                isPinned: true,
+                style: {
+                    backgroundColor: '#00d'
+                },
+                info: {
+                    txt: 'Fullstack Me Baby!'
+                }
+            },
+            {
+                id: 'n102',
+                createdAt: 1112223,
+                type: 'NoteImg',
+                isPinned: false,
+                info: {
+                    url: 'http://some-img/me',
+                    title: 'Bobi and Me'
+                },
+                style: {
+                    backgroundColor: '#00d'
+                }
+            },
+            {
+                id: 'n103',
+                createdAt: 1112224,
+                type: 'NoteTodos',
+                isPinned: false,
+                info: {
+                    title: 'Get my stuff together',
+                    todos: [
+                        { txt: 'Driving license', doneAt: null },
+                        { txt: 'Coding power', doneAt: 187111111 }
+                    ]
+                }
+            }
+        ]
+        saveToStorage(NOTE_KEY, notes)
+    }
+}
--- services/util.service.js
+++ services/util.service.js
@@ -1,14 +1,4 @@
-export const utilService = {
-    makeId,
-    makeLorem,
-    getRandomIntInclusive,
-    getRandomColor,
-    padNum,
-    getDayName,
-    getMonthName,
-}
-
-function makeId(length = 6) {
+export function makeId(length = 6) {
     var txt = ''
     var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 
@@ -19,7 +9,7 @@ function makeId(length = 6) {
     return txt
 }
 
-function makeLorem(size = 100) {
+export function makeLorem(size = 100) {
     var words = ['The sky', 'above', 'the port', 'was', 'the color of television', 'tuned', 'to', 'a dead channel', '.', 'All', 'this happened', 'more or less', '.', 'I', 'had', 'the story', 'bit by bit', 'from various people', 'and', 'as generally', 'happens', 'in such cases', 'each time', 'it', 'was', 'a different story', '.', 'It', 'was', 'a pleasure', 'to', 'burn']
     var txt = ''
     while (size > 0) {
@@ -29,17 +19,17 @@ function makeLorem(size = 100) {
     return txt
 }
 
-function getRandomIntInclusive(min, max) {
+export function getRandomIntInclusive(min, max) {
     min = Math.ceil(min)
     max = Math.floor(max)
     return Math.floor(Math.random() * (max - min + 1)) + min //The maximum is inclusive and the minimum is inclusive 
 }
 
-function padNum(num) {
+export function padNum(num) {
     return (num > 9) ? num + '' : '0' + num
 }
 
-function getRandomColor() {
+export function getRandomColor() {
     const letters = '0123456789ABCDEF'
     var color = '#'
     for (let i = 0; i < 6; i++) {
@@ -48,15 +38,25 @@ function getRandomColor() {
     return color
 }
 
-function getDayName(date, locale) {
+export function getDayName(date, locale) {
     date = new Date(date)
     return date.toLocaleDateString(locale, { weekday: 'long' })
 }
 
 
-function getMonthName(date) {
+export function getMonthName(date) {
     const monthNames = ["January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"
     ]
     return monthNames[date.getMonth()]
+}
+
+
+export function saveToStorage(key, val) {
+    localStorage.setItem(key, JSON.stringify(val))
+}
+
+export function loadFromStorage(key) {
+    var val = localStorage.getItem(key)
+    return JSON.parse(val)
 }
