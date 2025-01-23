4101486e8781e8ac024f91c0589aa3caa3884ae3
tamarNinaLevy <tamarlevy2002@gmail.com>
2025-01-23 07:55:40 +0200

    formatted date in preview mail

--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -4,10 +4,19 @@ export function MailPreview({ mail }) {
 
     const bold = !mail.isRead ? 'bold-font' : ''
 
+    function formatTime(millis) {
+        const date = new Date(millis)
+        const hours = date.getHours().toString().padStart(2, '0')
+        const minutes = date.getMinutes().toString().padStart(2, '0')
+        return `${hours}:${minutes}`
+    }
+
+    const time = formatTime(mail.createdAt)
+
     return <div className='preview-container' key={mail.id}>
         <span className={`span-margin ${bold}`}>{mail.from}</span> <br />
         <span className={`span-margin ${bold} font-gray`}>{mail.subject}</span> <br />
         <span className="span-margin">{txtBody}</span> <br />
-        <span className="span-margin">{mail.createdAt}</span> <br />
+        <span className="span-margin">{time}</span> <br />
     </div>
 }
