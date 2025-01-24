8f7d67eab30afa1aadf33ff839b1436441d523c8
tamarNinaLevy <tamarlevy2002@gmail.com>
2025-01-24 14:38:37 +0200

    added style to search input

--- apps/mail/cmps/SearchInput.jsx
+++ apps/mail/cmps/SearchInput.jsx
@@ -1,5 +1,5 @@
 export function SearchInput() {
     return <div className="search-container">
-        <input type="text" className="search" name="filter" id="filter" placeholder="search"/>
+        <input type="text" className="search" name="filter" id="filter" placeholder="Search mail"/>
     </div>
 }
