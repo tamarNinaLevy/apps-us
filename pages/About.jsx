555e5439aade94bf981b15c28ec4624fcff61f54
tamarNinaLevy <tamarlevy2002@gmail.com>
2025-01-25 22:17:32 +0200

    about-content

--- pages/About.jsx
+++ pages/About.jsx
@@ -1,6 +1,12 @@
+import { makeLorem } from "../services/util.service.js"
 
 export function About() {
-    return <section className="about">
-        <h1>About Page</h1>
+
+    const aboutPageHeader = 'About us'
+    const content = makeLorem(100)
+
+    return <section className="about flex column align-center">
+        <h1 className="about-header">{aboutPageHeader}</h1>
+        <div className="about-txt">{content}</div>
     </section>
 }
